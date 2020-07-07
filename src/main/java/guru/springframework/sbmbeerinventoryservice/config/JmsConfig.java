00573d58bcfee3affbf6293166df418032c41c00
Maria Mihai <maria.ic.mihai@gmail.com>
2020-07-07 18:03:31 +0200

    Challenge - Create New Inventory Listener. Closes #13

--- src/main/java/guru/springframework/sbmbeerinventoryservice/config/JmsConfig.java
+++ src/main/java/guru/springframework/sbmbeerinventoryservice/config/JmsConfig.java
@@ -1,20 +1,37 @@
 package guru.springframework.sbmbeerinventoryservice.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import guru.springframework.sbmbeerinventoryservice.events.NewInventoryEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 public class JmsConfig {
 
+    public static final String NEW_INVENTORY_QUEUE = "new-inventory";
+
     @Bean
-    public MessageConverter messageConverter() {
+    public MessageConverter messageConverter(ObjectMapper objectMapper) {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setTargetType(MessageType.TEXT);
         converter.setTypeIdPropertyName("_type");
+        converter.setTypeIdMappings(setClassMappings());
+
+        converter.setObjectMapper(objectMapper);
 
         return converter;
+    }
+
+    private Map<String,Class<?>> setClassMappings() {
+        Map<String,Class<?>> typeIdMappings = new HashMap<>();
+        typeIdMappings.put(NewInventoryEvent.class.getSimpleName(), NewInventoryEvent.class);
+
+        return typeIdMappings;
     }
 }
---
+++ src/main/java/guru/springframework/sbmbeerinventoryservice/services/inventory/NewInventoryListener.java
@@ -0,0 +1,34 @@
+package guru.springframework.sbmbeerinventoryservice.services.inventory;
+
+import guru.springframework.sbmbeerinventoryservice.config.JmsConfig;
+import guru.springframework.sbmbeerinventoryservice.domain.BeerInventory;
+import guru.springframework.sbmbeerinventoryservice.events.NewInventoryEvent;
+import guru.springframework.sbmbeerinventoryservice.repositories.BeerInventoryRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class NewInventoryListener {
+
+    private final BeerInventoryRepository beerInventoryRepository;
+    private final JmsTemplate jmsTemplate;
+
+    @Transactional
+    @JmsListener(destination = JmsConfig.NEW_INVENTORY_QUEUE)
+    public void listen(NewInventoryEvent event) {
+        log.debug("Getting request for a new inventory - " + event);
+
+        BeerInventory newBeerInventory = BeerInventory.builder()
+                .beerId(event.getBeerDto().getId())
+                .upc(event.getBeerDto().getUpc())
+                .quantityOnHand(event.getBeerDto().getQuantityOnHand())
+                .build();
+        beerInventoryRepository.save(newBeerInventory);
+    }
+}
